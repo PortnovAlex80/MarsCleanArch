608a6472d7d8524c4173a9cd928a3b0d697c561b
AlexPortnov80 <savinopoint@ya.ru>
2023-08-06 12:52:28 +0200

    fix rover tests

--- src/test/kotlin/com/marsrover/NasaMarsRover/domain/entities/RoverTest.kt
+++ src/test/kotlin/com/marsrover/NasaMarsRover/domain/entities/RoverTest.kt
@@ -1,17 +1,22 @@
 package com.marsrover.NasaMarsRover.domain.entities
 
+import com.marsrover.NasaMarsRover.rover.application.port.out.RoverIdGeneratorPort
 import com.marsrover.NasaMarsRover.rover.domain.Direction
 import com.marsrover.NasaMarsRover.rover.domain.Position
 import com.marsrover.NasaMarsRover.rover.domain.Rover
+import com.marsrover.NasaMarsRover.rover.domain.RoverId
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicLong
 
 class RoverTest {
     @Test
     fun testRoverInitialState() {
         val initialPosition = Position(5, 5)
         val initialDirection = Direction.NORTH
-        val rover = Rover(initialPosition, initialDirection)
+        val counter = AtomicLong(0)
+        val roverId = RoverId(counter.incrementAndGet())
+        val rover = Rover(roverId, initialPosition, initialDirection)
 
         assertEquals(initialPosition, rover.position)
         assertEquals(initialDirection, rover.direction)
