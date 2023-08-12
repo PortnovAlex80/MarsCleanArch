dbba080a4d8f38d9f6d61da2e0b949de92121d5f
AlexPortnov80 <savinopoint@ya.ru>
2023-08-12 21:07:56 +0200

    add Extractor inMemory

--- rover/adapters/src/main/kotlin/com/marsrover/nasa/rover/adapter/out/persistence/inmemory/InMemoryRoverPersistenceAdapter.kt
+++ rover/adapters/src/main/kotlin/com/marsrover/nasa/rover/adapter/out/persistence/inmemory/InMemoryRoverPersistenceAdapter.kt
@@ -1,17 +1,23 @@
 package com.marsrover.nasa.rover.adapter.out.persistence.inmemory
 
+import com.marsrover.nasa.rover.application.port.`in`.RoverExtractorPortIn
 import com.marsrover.nasa.rover.application.port.out.CreateRoversPortOut
 import com.marsrover.nasa.rover.domain.Rover
 import com.marsrover.nasa.rover.domain.RoverId
 
-class InMemoryRoverPersistenceAdapter : CreateRoversPortOut {
+class InMemoryRoverPersistenceAdapter : CreateRoversPortOut, RoverExtractorPortIn {
     private val inMemoryStorage = mutableMapOf<RoverId, Rover>()
     override fun createRoversSave(rovers: List<Rover>) {
         for (rover in rovers) {
             inMemoryStorage.put(rover.roverId, rover)
         }
     }
-    override fun readRovers(): List<Rover> {
+
+    override fun getRoverById(id: RoverId): Rover? {
+        return inMemoryStorage[id]
+    }
+
+    override fun getAllRovers(): List<Rover>? {
         return inMemoryStorage.values.toList()
     }
 }
--- rover/usecases/src/main/kotlin/com/marsrover/nasa/rover/application/port/in/RoverExtractorPortIn.kt
+++ rover/usecases/src/main/kotlin/com/marsrover/nasa/rover/application/port/in/RoverExtractorPortIn.kt
@@ -3,7 +3,7 @@ package com.marsrover.nasa.rover.application.port.`in`
 import com.marsrover.nasa.rover.domain.Rover
 import com.marsrover.nasa.rover.domain.RoverId
 
-interface etRoversFromPortsIn {
-    fun getAllRovers(): List<Rover>
-    fun getRoverById(id: RoverId)
+interface RoverExtractorPortIn {
+    fun getAllRovers(): List<Rover>?
+    fun getRoverById(id: RoverId): Rover?
 }
